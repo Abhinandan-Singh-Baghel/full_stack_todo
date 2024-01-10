85eaa0833c3f2494cf55d05c93e80f3dc3d03ed8
Abhinandan Singh Baghel <abhinandansinghbaghel2001@gmail.com>
2024-01-10 15:45:31 +0530

    Database part completed

--- backend/db.js
+++ backend/db.js
@@ -2,7 +2,7 @@ const mongoose = require("mongoose");
 
 // mongodb+srv://abhinandansinghbaghel2001:<password>@cluster0.3fjqkhz.mongodb.net/
 
-mongoose.connect("mongodb+srv://abhinandansinghbaghel2001:<password>@cluster0.3fjqkhz.mongodb.net/new_Year_project");
+mongoose.connect("mongodb+srv://abhinandansinghbaghel2001:cYfhJfMRLe23S7Ln@cluster0.3fjqkhz.mongodb.net/new_Year_project");
 
 
 const todoSchema = mongoose.Schema({
--- backend/index.js
+++ backend/index.js
@@ -17,7 +17,7 @@ app.post("/todo", async function(req, res){
     const createPayload = req.body;
     const parsedPayload = createTodo.safeParse(createPayload);
 
-    if(!parsePayload.success){
+    if(!parsedPayload.success){
         res.status(411).json({
             msg: "You sent the wrong inputs",
         })
