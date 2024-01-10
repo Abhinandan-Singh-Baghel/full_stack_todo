60ceee11ce87943cc29f41a9e9e331351eedf706
Abhinandan Singh Baghel <abhinandansinghbaghel2001@gmail.com>
2024-01-10 16:03:16 +0530

    frontend

--- frontend/src/components/CreateTodo.jsx
+++ frontend/src/components/CreateTodo.jsx
@@ -1,8 +1,17 @@
 export function CreateTodo(){
     return <div>
-        <input type="text" placeholder="title"></input>
-        <input type="text" placeholder="description"></input>
+        <input style={{
+            padding: 10,
+            margin: 10
+        }} type="text" placeholder="title"></input>
+        <input style={{
+            padding: 10,
+            margin: 10
+        }} type="text" placeholder="description"></input>
 
-        <button>Add a todo</button>
+        <button style={{
+            padding: 10,
+            margin: 10
+        }}>Add a todo</button>
     </div>
 }
---
+++ frontend/src/components/Todos.jsx
@@ -0,0 +1,7 @@
+export function Todos(){
+    return <div>
+        <h1> Go to the gym</h1>
+        <h1>Hit the Gym</h1>
+        <button>Mark as completed</button>
+    </div>
+} 
