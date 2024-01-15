3ffc8434e6ae4bec1251976fb0ea58a6647b6bbb
Abhinandan Singh Baghel <abhinandansinghbaghel2001@gmail.com>
2024-01-15 12:15:28 +0530

    Memoization ugly sol

--- re-rendering/re-rendering/src/App.jsx
+++ re-rendering/re-rendering/src/App.jsx
@@ -3,57 +3,29 @@ import reactLogo from './assets/react.svg'
 import viteLogo from '/vite.svg'
 import './App.css'
 
-let counter = 4;
 
 function App(){
+  const [counter, setCounter] = useState(0);
+  const [inputValue, setInputValue] = useState(1);
 
-  const [todos, setTodos] = useState([{
-    id: 0,
-    title: "go to gym",
-    description: "go to gym from 1-2"
-  },
-  {
-    id: 1,
-    title: "eat food",
-    description: "eat food from 2-4"
-  },
-  {
-    id: 2,
-    title: "study math",
-    description: "study math from 4-6"
-  }]);
+  let count = 0;
 
-  const [counter, setCouter] = useState(0);
+  for(let i = 1; i<=inputValue; i++){
+    count = count + i;
 
-  function increaseCount(){
-    setCouter(counter + 1);
   }
-
-  const filteredTodos = todos.filter(x => x.id % 2 == 0);
-
-  return (
-    <div>
-      <button onClick={increaseCount} Increase Count ></button>
-      {filteredTodos.map(todo => <Todo title = {todo.title} description= {todo.description}></Todo>)}
-    </div>
-  )
-
-}
-
-
-
-const Todo = React.memo(function({title, description}){
   return <div>
-    <h1>
-      {title}
-    </h1>
-    <h3>
-      {description}
-    </h3>
+    <input onChange={function(e){
+      setInputValue(e.target.value);
+    }} placeholder={"Find sum from 1 to n"}></input>
+    <br />
+    Sum from 1 to {inputValue} is {count}
+    <br/>
+    <button onClick={()=>{
+      setCounter(counter + 1);
+    }}>Counter ({counter})</button>
   </div>
-})
-
-
+}
 
 
 
