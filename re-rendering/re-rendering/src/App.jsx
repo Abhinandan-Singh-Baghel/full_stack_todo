0753c0e7df97596bbf3d824e32cd9321f1e78e4c
Abhinandan Singh Baghel <abhinandansinghbaghel2001@gmail.com>
2024-01-17 13:05:44 +0530

    useRef

--- re-rendering/re-rendering/src/App.jsx
+++ re-rendering/re-rendering/src/App.jsx
@@ -1,40 +1,31 @@
-import React, { useCallback, useEffect, useState } from 'react'
+import React, { useCallback, useRef, useEffect, useState, useLayoutEffect } from 'react'
 import {memo} from 'react'
 import reactLogo from './assets/react.svg'
 import viteLogo from '/vite.svg'
 import './App.css'
 
 
-function App(){
-  const [todos, setTodos] = useState([]);
 
-  useEffect(()=>{
-    fetch("https://sum-server.100xdevs.com/todos")
-      .then(async function(res){
-        const json = await res.json();
-        setTodos(json.todos); 
-      })
-  }, [])
+function App() {
+  const divRef = useRef();
 
-  return <div>
-   { todos.map(todo => <Todo key = {todo.id} title = {todo.title} description={todo.description}></Todo>)}
-  </div>
+  useEffect(() => {
+    setTimeout(()=>{
+      divRef.current.innerHTML = "10"
+
+    },5000);
+  },[])
+
+  const incomeTax = 20000;
+  
+
+  return (
+    <div>
+      hi there , your income tax returns are <div ref = {divRef}>{incomeTax}</div>
+      
+    </div>
+  )
 }
 
-
-
-function Todo({id, title, description}){
-  return <div>
-    <h1>
-      {id}
-    </h1>
-    <h1>
-      {title}
-    </h1>
-    <h4>
-      {description}
-    </h4>
-  </div>
-}
 
 export default App
